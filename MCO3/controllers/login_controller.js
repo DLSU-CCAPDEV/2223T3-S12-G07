7be51e4f20cdd5dd57065fae8c8723f7b4aa5022
rpineda26 <91407317+rpineda26@users.noreply.github.com>
2023-08-03 17:26:26 +0800

    add: connected login controller to middleware, next up: chagne signup.js code logic to incorporate the middleware error messages

--- MCO3/controllers/login_controller.js
+++ MCO3/controllers/login_controller.js
@@ -2,7 +2,7 @@ const db  = require('../models/db.js');
 const User = require('../models/UserModel.js');
 // import module `bcrypt`
 const bcrypt = require('bcrypt');
-
+const { validationResult } = require('express-validator');
 const loginController ={
     getLogin: function(req, res){
         if(req.session.flag){
@@ -11,6 +11,10 @@ const loginController ={
             res.render('login');
     },
     postLogin: async function(req, res){
+        var errors = validationResult(req);
+        if(errors.isEmpty()){
+
+        
         var username = req.body.username;
         var password = req.body.password;
         var query = {userName: username};
@@ -38,6 +42,13 @@ const loginController ={
 
         }else{
             res.status(404).render('login');
+        }}else{
+            var details = {};
+            errors = errors.errors;
+            for(i=0;i<errors.length;i++){
+                details[errors[i].param+'_error'] = errors[i].msg;
+            }
+            res.render('login', details);
         }
     },
 };
