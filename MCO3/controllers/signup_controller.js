398a3370e8a8c0712540fc6c7b2ca606d45f5f62
rpineda26 <91407317+rpineda26@users.noreply.github.com>
2023-08-03 17:22:13 +0800

    add: connecting validation middleware to signup controller,next update will be on connecting the login controller to the validation middleware

--- MCO3/controllers/signup_controller.js
+++ MCO3/controllers/signup_controller.js
@@ -3,6 +3,7 @@ const User = require("../models/UserModel.js");
 // import module `bcrypt`
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const { validationResult } = require('express-validator');
 const signupController = {
 
     getSignUp:  function (req, res) {
@@ -18,46 +19,56 @@ const signupController = {
                 alert('You have been logged out from active sessions');
             });
         }else{
-            var firstName = req.body.firstName;
-            var userName = req.body.username;
-            var lastName = req.body.lastName;
-            var password= req.body.password;
-            var email= req.body.email;
-            var contactNumber ="";
-            if(req.body.contact_number != null)
-                contactNumber= req.body.contact_number;
-            var idNumber= req.body.idnumber;
+            var errors = validationResult(req);
+            if(errors.isEmpty()){
+                var firstName = req.body.firstName;
+                var userName = req.body.username;
+                var lastName = req.body.lastName;
+                var password= req.body.password;
+                var email= req.body.email;
+                var contactNumber ="";
+                if(req.body.contact_number != null)
+                    contactNumber= req.body.contact_number;
+                var idNumber= req.body.idnumber;
 
-            bcrypt.hash(password, saltRounds, async function(err, hash) {
-            var user = {
-                firstName: firstName,
-                lastName: lastName,
-                userName: userName,
-                password: hash,
-                email: email,
-                contactNumber: contactNumber,
-                idNumber: idNumber,
-            };
-            var result = await db.insertOne(User,user);
-            if(result!=null){
-                console.log("User inserted");
-                    data = {
-                        userName: userName,
-                        firstName: firstName,
-                        lastName: lastName,
-                    };
-                    req.session.flag = true;
-                
-                req.session.user = data;
-                res.redirect('/profile_page?userName='+userName);
+                bcrypt.hash(password, saltRounds, async function(err, hash) {
+                var user = {
+                    firstName: firstName,
+                    lastName: lastName,
+                    userName: userName,
+                    password: hash,
+                    email: email,
+                    contactNumber: contactNumber,
+                    idNumber: idNumber,
+                };
+                var result = await db.insertOne(User, user);
+                if(result!=null){
+                    console.log("User inserted");
+                        data = {
+                            userName: userName,
+                            firstName: firstName,
+                            lastName: lastName,
+                        };
+                        req.session.flag = true;
+                    
+                    req.session.user = data;
+                    res.redirect('/profile_page?userName='+userName);
+                }else{
+                        console.log("failed register");
+                        req.session.flag = false;
+                        res.render('register');
+                }
+
+                });
+                //create user from the initiated v
             }else{
-                    console.log("failed register");
-                    req.session.flag = false;
-                    res.render('register');
+                errors = errors.errors;
+                var details = {};
+                for (i = 0;i <errors.length;i++){
+                    details[errors[i].param+'_error'] = errors[i].msg;
+                }
+                res.render('register', details);
             }
-
-        });
-            //create user from the initiated v
         }
             
     },
